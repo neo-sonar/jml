72ccac6f754bdfdbf86f62e2301b2a5e5097b817
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-10-20 03:40:43 +0200

    Maybe fix CodeTokeniser on MSVC

--- module/lua_juce_gui_extra/code_editor/CodeTokeniser.cpp
+++ module/lua_juce_gui_extra/code_editor/CodeTokeniser.cpp
@@ -3,8 +3,8 @@ namespace lua_juce {
 auto juce_CodeTokeniser(sol::table& state) -> void
 {
     auto tokeniser                      = state.new_usertype<juce::CodeTokeniser>("InternalCodeTokeniser", sol::no_constructor);
-    tokeniser["readNextToken"]          = LUA_JUCE_C_CALL(&juce::CodeTokeniser::readNextToken);
-    tokeniser["getDefaultColourScheme"] = LUA_JUCE_C_CALL(&juce::CodeTokeniser::getDefaultColourScheme);
+    tokeniser["readNextToken"]          = &juce::CodeTokeniser::readNextToken;
+    tokeniser["getDefaultColourScheme"] = &juce::CodeTokeniser::getDefaultColourScheme;
 }
 
 } // namespace lua_juce
