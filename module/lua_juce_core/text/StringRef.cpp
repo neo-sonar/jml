c3e062f600a9a818e00958163bb7535da952e350
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-10-22 16:31:09 +0200

    [core] Cleanup StringRef

--- module/lua_juce_core/text/StringRef.cpp
+++ module/lua_juce_core/text/StringRef.cpp
@@ -12,6 +12,6 @@ auto juce_StringRef(sol::table& state) -> void
     str["isEmpty"]    = LUA_JUCE_C_CALL(&juce::StringRef::isEmpty);
     str["isNotEmpty"] = LUA_JUCE_C_CALL(&juce::StringRef::isNotEmpty);
     str["length"]     = LUA_JUCE_C_CALL(&juce::StringRef::length);
-    str["text"]       = LUA_JUCE_C_CALL(&juce::StringRef::text);
+    str["text"]       = &juce::StringRef::text;
 }
 } // namespace lua_juce
